b0c53d3fa1d301833db0c958133e6c2f9cfdb9e7
Kaylan Smith <kaylansmith@Kaylans-MacBook-Pro.local>
2016-03-28 09:47:44 -0500

    needs additional styling, functionality there

--- js/app-1.js
+++ js/app-1.js
@@ -12,7 +12,7 @@ import fetch from "isomorphic-fetch"
 // the following line, if uncommented, will enable browserify to push
 // a changed fn to you, with source maps (reverse map from compiled
 // code line # to source code line #), in realtime via websockets
-// -- browserify-hmr having install issues right undonew
+// -- browserify-hmr having install issues right now
 // if (module.hot) {
 //     module.hot.accept()
 //     module.hot.dispose(() => {
@@ -20,56 +20,70 @@ import fetch from "isomorphic-fetch"
 //     })
 // }
 
+// Check for ServiceWorker support before trying to install it
+// if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.register('./serviceworker.js').then(() => {
+//         // Registration was successful
+//         console.info('registration success')
+//     }).catch(() => {
+//         console.error('registration failed')
+//             // Registration failed
+//     })
+// } else {
+//     // No ServiceWorker Support
+// }
 
 import DOM from 'react-dom'
 import React, {Component} from 'react'
 import Backbone from 'backbone'
 
 
-
 function app() {
-    // --------------------- Model/Collection --------------------- //
-    var ListModel = Backbone.Model.extend({
+	// --------------------- Model/Collection --------------------- //
+	var ListModel = Backbone.Model.extend ({
+		initialize: function(newListItem) {
+			this.set({item: newListItem})
+		},
 
-        defaults: {
+		defaults: {
             status: "pending"
-        },
+		}
 
-        initialize: function(newItem) {
-            this.set({item: newItem})
-        }
-    })
+	})
 
-    var ListCollection = Backbone.Collection.extend({
-        model: ListModel
-    })
+	var ListCollection = Backbone.Collection.extend ({
+		model: ListModel
+	})
 
-    // --------------------- Views --------------------- //
+	// --------------------- Views --------------------- //
 
     var ListView = React.createClass({
-        // gets item passed in from _handleKeyDown function below
-        // add new instance of model with item to collection
-        // list coll was put on state
+    	// gets item passed in from _handleKeyDown function below
+    	// add new instance of model with item to collection
+    	// list coll was put on state
         _addItem: function(item) {
+            // console.log(this)
             this.state.listColl.add(new ListModel(item))
-            this._updater()
         },
 
         _genbuttons: function() {
-            var butts = ["pending","undone","done","all"].map(function(itemStatus){
-                return <button onClick={this._filterView} value={itemStatus}>{itemStatus}</button>
+            var butts = ["pending","done","undone","all"].map(function(itemType){
+                return <button onClick={this._filterView} value={itemType}>{itemType}</button>
             }.bind(this))
             return butts
         },
 
         _filterView: function(event) {
+            // bug 
+            console.log(this)
             var buttView = event.target.value
             this.setState({
                 viewType: buttView
             })
         },
 
-        _removeItem: function(model) {
+        _removeItem: function() {
+            console.log(this.state.listColl)
             this.state.listColl.remove(model)
             this._updater()
         },
@@ -83,7 +97,7 @@ function app() {
         // list collection is on props, but moved to state above
         getInitialState: function() {
             return {
-                listColl: this.props.listColl,
+                listColl: this.props.listColl, 
                 viewType: "all"
             }
         },
@@ -93,20 +107,19 @@ function app() {
             var listColl = this.state.listColl
             if (this.state.viewType === "pending") listColl = listColl.where({status:"pending"})
             if (this.state.viewType === "done") listColl = listColl.where({status:"done"})
-            if (this.state.viewType === "undone") listColl = listColl.where({status:"undone"})
-
+            if (this.state.viewType === "undone") listColl = listColl.where({status:"undone"})            
             return (
                 <div className="listView">
                     <div className="buttons">{this._genbuttons()}</div>
                     <ItemAdder adderFunc={this._addItem}/>
-                    <ToDoList updater={this._updater} listColl={listColl} remover={this._removeItem}/>
+                    <ToDoList updater={this._updater} listColl={this.state.listColl} remover={this._removeItem}/>
                 </div>  
                 )
         }
     })
 
-    var ItemAdder = React.createClass({
-        // captures to-do item at input
+ 	var ItemAdder = React.createClass({
+ 		// captures to-do item at input
         _handleKeyDown: function(keyEvent) {
             if (keyEvent.keyCode === 13) {
                 var itemName = keyEvent.target.value
@@ -114,7 +127,6 @@ function app() {
                 keyEvent.target.value = ''
             }
         },
-
         // input runs _handleKeyDown, above 
         render: function() {
             return <input onKeyDown={this._handleKeyDown} />
@@ -122,16 +134,13 @@ function app() {
     })
 
     var ToDoList = React.createClass({
-
         _makeItem: function(model) {
             return <Item updater={this.props.updater} listModel={model} remover={this.props.remover}/>
         },
 
-
         render: function() {
-            console.log(this)
             return (
-                <div className="itemList">
+                <div className="toDoList">
                     {this.props.listColl.map(this._makeItem)}
                 </div>
                 )
@@ -139,7 +148,6 @@ function app() {
     })
 
     var Item = React.createClass({
-
         _selectStatus: function(event) {
             var newStat = event.target.value
             this.props.listModel.set({status:newStat})
@@ -151,38 +159,42 @@ function app() {
         },
 
         render: function() {
-            return <div className="item">
+             //return <p>{this.props.listModel.get('item')}</p>
+            return <div className="list">
                         <p>{this.props.listModel.get('item')}</p>
                         <p>{this.props.listModel.get('status')}</p>
                         <select onChange={this._selectStatus} >
-                            <option value="">change rsvp</option>
+                            <option value="">change item</option>
                             <option value="pending">pending</option>
-                            <option value="undone">undone</option>
                             <option value="done">done</option>
+                            <option value="undone">undone</option>
                         </select>
                         <button onClick={this._clickHandler}>x</button>
                     </div>
         }
     })
 
-    // --------------------- Router --------------------- //
+	// --------------------- Router --------------------- //
 
-    var Router = Backbone.Router.extend({
-        routes: {
-            "*default": "home"
-        },
+	var Router = Backbone.Router.extend ({
+		// one default route, home
+		routes: {
+			"*default" : "home"
+		},
 
-        home: function() {
-            DOM.render(<ListView listColl={new ListCollection()}/>,document.querySelector('.container'))
-        },
+		// home function places instance of the listCollection on the view, ListView
+		home: function() {
+			window.location.hash = "home"
+			DOM.render(<ListView listColl={new ListCollection()}/>, document.querySelector('.container'))
+		},
 
-        // home function places instance of the listCollection on the view, ListView
-        initialize: function() {
-            Backbone.history.start()
-        }
-    })
+		initialize: function() {
+			Backbone.history.start()
+		}
 
-    var rtr = new Router()
+	})
+
+	var rtr = new Router()
 
 }
 
